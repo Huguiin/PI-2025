4c42557977b78959643d19d047b07901222a73e5
Lavinia Oliveira <167258413+Niascreate@users.noreply.github.com>
2025-05-18 21:49:53 -0300

    Delete PI-2025 directory

--- PI-2025/JS/cart.js
+++
@@ -1,8 +0,0 @@
-function toggleCart() {
-  const cartSidebar = document.getElementById('cart-sidebar');
-  cartSidebar.classList.toggle('active');
-}
-
-// Attach the toggleCart function to the cart icon and close button
-document.getElementById('cart-icon').addEventListener('click', toggleCart);
-document.getElementById('close-cart-btn').addEventListener('click', toggleCart);
